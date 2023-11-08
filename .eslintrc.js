d7db1e33a14b74d76502edbab4a94c2379b7f282
Miguel Ángel Ramírez Ruiz <mugui098@gmail.com>
2023-11-08 16:00:34 +0100

    chores: add prettierrc and eslint jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-  env: {
-    node: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+    env: {
+        node: true,
+        es2021: true,
+        'jest/globals': true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
 };
